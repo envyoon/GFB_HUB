5b410f40941a3446f11d4e1f5f2361a5da4b2629
envyoon <krafftdj@daum.net>
2025-02-02 21:00:35 +0900

    Initial commit - 프로젝트 생성

---
+++ GFB_HUB/src/main/java/com/football/GfbHubApplication.java
@@ -0,0 +1,13 @@
+package com.football;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfbHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfbHubApplication.class, args);
+	}
+
+}
