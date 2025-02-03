0d73c7de2ba162baaffb0fb4a8d96d61b5ece86c
envyoon <krafftdj@daum.net>
2025-02-03 09:49:43 +0900

    Swagger 오류로 인한 버전 업데이트 및 Swagger 설정 추가
    버전 변경 : 2.4.0 -> 2.7.0
    Controller에 Tag및 Operation 추가.

--- GFB_HUB/src/main/java/com/football/domain/test/controller/TestController.java
+++ GFB_HUB/src/main/java/com/football/domain/test/controller/TestController.java
@@ -4,11 +4,15 @@ import com.football.common.dto.CommonResponse;
 import com.football.domain.test.dto.TestRequestDto;
 import com.football.domain.test.dto.TestResponseDto;
 import com.football.domain.test.service.TestService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Test API", description = "테스트 관련 API")
 @RestController
 @RequestMapping("/api/tests")
 @RequiredArgsConstructor
@@ -17,12 +21,14 @@ public class TestController {
     
     
     // Get 요청 테스트(전체) - http://localhost:8080/api/tests
+    @Operation(summary = "모든 테스트 데이터 조회", description = "DB에 저장된 모든 테스트 데이터를 조회합니다.")
     @GetMapping
     public CommonResponse<List<TestResponseDto>> getAllTests() {
         return CommonResponse.success(testService.getAllTests());
     }
     
     // Get 요청 테스트(단건) - http://localhost:8080/api/tests/{id}
+    @Operation(summary = "특정 테스트 데이터 조회", description = "ID를 이용하여 특정 테스트 데이터를 조회합니다.")
     @GetMapping("/{id}")
     public CommonResponse<TestResponseDto> getTestById(@PathVariable("id") Long id) {
         return CommonResponse.success(testService.getTestById(id));
@@ -30,6 +36,7 @@ public class TestController {
     
     // Get 요청 테스트 - http://localhost:8080/api/tests
     // Body (JSON) { "name" : "테스트 데이터 "}
+    @Operation(summary = "테스트 데이터 추가", description = "새로운 테스트 데이터를 DB에 추가합니다.")
     @PostMapping
     public CommonResponse<Long> createTest(@RequestBody TestRequestDto testDto) {
         testService.createTest(testDto);
@@ -37,6 +44,7 @@ public class TestController {
     }
     
     // Delete 요청 테스트 - http://localhost:8080/api/tests/{id}
+    @Operation(summary = "테스트 데이터 삭제", description = "ID를 이용하여 특정 테스트 데이터를 삭제합니다.")
     @DeleteMapping("/{id}")
     public CommonResponse<Void> deleteTest(@PathVariable("id") Long id) {
         testService.deleteTest(id);
@@ -45,6 +53,7 @@ public class TestController {
     
     // Put 요청 테스트 - http://localhost:8080/api/tests/{id}
     // Body (JSON) { "name" : "변경된 테스트 데이터 "}
+    @Operation(summary = "테스트 데이터 수정", description = "ID를 이용하여 특정 테스트 데이터를 수정합니다.")
     @PutMapping("/{id}")
     public CommonResponse<Void> updateTest(@PathVariable("id") Long id, @RequestBody TestRequestDto testDto) {
         testService.updateTest(id, testDto);
