2c7b5306c9a20858bddabf4bc951220d654c4c91
envyoon <krafftdj@daum.net>
2025-02-24 16:43:59 +0900

    main log 변경

--- GFB_HUB/src/main/java/com/football/GfbHubApplication.java
+++ GFB_HUB/src/main/java/com/football/GfbHubApplication.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.football.common.utils.MailService;
+
 
 @SpringBootApplication
 public class GfbHubApplication {
 	
-	private static final Logger Log = LoggerFactory.getLogger(GfbHubApplication.class);
+	private static final Logger Log = LoggerFactory.getLogger(MailService.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(GfbHubApplication.class, args);
